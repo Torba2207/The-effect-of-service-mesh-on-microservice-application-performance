ff43d465c673f32e17bf5a86d1c6c5a60406262f
KiriXiwi <169109577+KiriXiwi@users.noreply.github.com>
2026-06-04 21:06:19 +0300

    Changes in integration and differential logic of the app

--- ServiceMeshTestApplication/SharedModels/Models/DifferentialRequest.cs
+++ ServiceMeshTestApplication/SharedModels/Models/DifferentialRequest.cs
@@ -5,6 +5,6 @@ public class DifferentialRequest
     public string Function { get; set; } = "x^2";
     public double InitialConditionX { get; set; } = 0.0;
     public double InitialConditionY { get; set; } = 0.0;
-    public double Range { get; set; } = 1.0;
+    public double Range { get; set; } = 0.0;
     public int Steps { get; set; } = 5;
 }
